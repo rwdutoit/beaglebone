805c3a23b3fd77607cc58e81a8887c9554a9383c
rwdutoit <rwdutoit@gmail.com>
2017-03-28 08:02:44 +0200

    Create cgi.cpp

---
+++ cgi.cpp
@@ -0,0 +1,29 @@
+/* C++ CGI BeagleBone uptime example -- Written by Derek Molloy (www.derekmolloy.ie) */
+
+#include <iostream>         // for the input/output
+#include <stdlib.h>         // for the getenv call
+#include <sys/sysinfo.h>    // for the system uptime call
+using namespace std;
+
+int counter=0;
+int main(){
+   struct sysinfo info;                           // A structure that contains system stats
+   sysinfo(&info);                                // retrieve the data
+   counter++;
+   char *value = getenv("REMOTE_ADDR");           // The remote address CGI environment variable
+   cout << "Content-type:text/html\r\n ";      // Generate the HTML output
+   cout << "Refresh:1\r\n\r\n";      // Refresh
+   cout << "<html><head>\n";
+cout << "<META HTTP-EQUIV='refresh' CONTENT='1'>";
+   cout << "<title>EBB C++ Uptime</title>\n";
+   cout << "</head><body>\n";
+   cout << "<h1>BeagleBone System Uptime</h1>\n";
+   int mins = info.uptime / 60;                   // the uptime comes from the sysinfo struct
+   int ram = info.freeram / 1024 / 1024;          // the available memory in Mb
+   cout << "<div> The BBB system uptime is " << mins << " minutes.\n";
+   cout << "There is " << ram << " Mb of memory available.</div>\n";
+   cout << "counter: " << counter << " ";
+   cout << "<div> The CGI REMOTE_ADDR environment variable is " << value << "</div>";
+   cout << "</body></html>\n";
+   return 0;
+}
